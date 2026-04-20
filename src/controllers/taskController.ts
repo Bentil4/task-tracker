910623e97bbff47bd0b165539b24e155c6f657e4
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-04-20 08:25:58 +0000

    feat: refactor taskController and add user route for registration

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,16 +1,6 @@
 import Joi from "joi";
-import { NextFunction, Request, RequestHandler, Response } from "express";
 import { TaskModel } from "../models/Task";
-
-export class HttpError extends Error {
-  constructor(
-    public readonly statusCode: number,
-    message: string,
-  ) {
-    super(message);
-    this.name = "HttpError";
-  }
-}
+import { HttpError, validate, sendSuccess, wrapAsync } from "../utils/helper";
 
 const titleSchema = Joi.string().trim().min(1).messages({
   "string.base": "'title' must be a string.",
@@ -37,38 +27,6 @@ const updateSchema = Joi.object({
   .messages({
     "object.min": "Body must include at least 'title' or 'completed'.",
   });
-
-const validate = <T>(schema: Joi.ObjectSchema<T>, payload: unknown): T => {
-  const { error, value } = schema.validate(payload, {
-    abortEarly: false,
-    allowUnknown: false,
-    stripUnknown: true,
-  });
-  if (error)
-    throw new HttpError(400, error.details.map((d) => d.message).join("; "));
-  return value;
-};
-
-const sendSuccess = <T>(
-  res: Response,
-  statusCode: number,
-  data: T,
-  message: string,
-): void => {
-  res.status(statusCode).json({ success: true, message, data });
-};
-
-const wrapAsync = (
-  fn: (req: Request, res: Response) => Promise<void>,
-): RequestHandler => {
-  return async (req, res, next: NextFunction) => {
-    try {
-      await fn(req, res);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 export const taskController = {
   getAllTasks: wrapAsync(async (_req, res) => {
