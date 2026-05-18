b4acdcf996dca7c96d50d577119a3e6aab52b744
Bentil4 <bentilfrederick4@gmail.com>
2026-05-18 09:07:43 +0000

    refactor: remove Joi validation and related error handling from taskController

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,74 +1,7 @@
-import Joi from "joi";
-import { NextFunction, Request, RequestHandler, Response } from "express";
 import { TaskModel } from "../models/Task";
-
-export class HttpError extends Error {
-  constructor(
-    public readonly statusCode: number,
-    message: string,
-  ) {
-    super(message);
-    this.name = "HttpError";
-  }
-}
-
-const titleSchema = Joi.string().trim().min(1).messages({
-  "string.base": "'title' must be a string.",
-  "string.empty": "'title' must be a non-empty string.",
-  "string.min": "'title' must be a non-empty string.",
-});
-
-const createSchema = Joi.object({
-  title: titleSchema
-    .required()
-    .messages({ "any.required": "'title' is required." }),
-  completed: Joi.boolean()
-    .default(false)
-    .messages({ "boolean.base": "'completed' must be a boolean." }),
-});
-
-const updateSchema = Joi.object({
-  title: titleSchema,
-  completed: Joi.boolean().messages({
-    "boolean.base": "'completed' must be a boolean.",
-  }),
-})
-  .min(1)
-  .messages({
-    "object.min": "Body must include at least 'title' or 'completed'.",
-  });
-
-const validate = <T>(schema: Joi.ObjectSchema<T>, payload: unknown): T => {
-  const { error, value } = schema.validate(payload, {
-    abortEarly: false,
-    allowUnknown: false,
-    stripUnknown: true,
-  });
-  if (error)
-    throw new HttpError(400, error.details.map((d) => d.message).join("; "));
-  return value;
-};
-
-const sendSuccess = <T>(
-  res: Response,
-  statusCode: number,
-  data: T,
-  message: string,
-): void => {
-  res.status(statusCode).json({ success: true, message, data });
-};
-
-const wrapAsync = (
-  fn: (req: Request, res: Response) => Promise<void>,
-): RequestHandler => {
-  return async (req, res, next: NextFunction) => {
-    try {
-      await fn(req, res);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
+import { HttpError } from "../utils/errors";
+import { validate, sendSuccess, wrapAsync } from "../utils/helpers";
+import { createSchema, updateSchema } from "../validators/taskValidator";
 
 export const taskController = {
   getAllTasks: wrapAsync(async (_req, res) => {
